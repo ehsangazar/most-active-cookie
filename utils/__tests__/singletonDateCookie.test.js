b201d2aadd00940e8e90342b8b6807a3d870a9e7
Ehsan Gazar <me@ehsangazar.com>
2024-03-14 19:09:00 +0100

    feat: more test scenarios

--- utils/__tests__/singletonDateCookie.test.js
+++ utils/__tests__/singletonDateCookie.test.js
@@ -25,4 +25,68 @@ describe("singletonDateCookie", () => {
       },
     ]);
   });
+
+  it("should return count 2", () => {
+    const dateCookieObj = singletonDateCookie.getInstance();
+    dateCookieObj.clearInstance();
+    dateCookieObj.addCookieLine("4sMM2LxV07bPJzwf,2018-12-07T23:30:00+00:00");
+    dateCookieObj.addCookieLine("4sMM2LxV07bPJzwf,2018-12-07T23:30:00+00:00");
+    const result = dateCookieObj.getAllResultForSpecificDate("2018-12-07");
+    expect(result).toEqual([
+      {
+        cookie: "4sMM2LxV07bPJzwf",
+        count: 2,
+      },
+    ]);
+  });
+
+  it("should return count 1 for different dates", () => {
+    const dateCookieObj = singletonDateCookie.getInstance();
+    dateCookieObj.clearInstance();
+    dateCookieObj.addCookieLine("4sMM2LxV07bPJzwf,2018-12-07T23:30:00+00:00");
+    dateCookieObj.addCookieLine("4sMM2LxV07bPJzwf,2018-12-08T23:30:00+00:00");
+    const result1 = dateCookieObj.getAllResultForSpecificDate("2018-12-07");
+    const result2 = dateCookieObj.getAllResultForSpecificDate("2018-12-08");
+    expect(result1).toEqual([
+      {
+        cookie: "4sMM2LxV07bPJzwf",
+        count: 1,
+      },
+    ]);
+    expect(result2).toEqual([
+      {
+        cookie: "4sMM2LxV07bPJzwf",
+        count: 1,
+      },
+    ]);
+  });
+  it("should return count 1 and 2 for different dates", () => {
+    const dateCookieObj = singletonDateCookie.getInstance();
+    dateCookieObj.clearInstance();
+    dateCookieObj.addCookieLine("4sMM2LxV07bPJzwf,2018-12-07T23:30:00+00:00");
+    dateCookieObj.addCookieLine("4sMM2LxV07bPJzwf,2018-12-07T23:30:00+00:00");
+    dateCookieObj.addCookieLine("4sMM2LxV07bPJzwf,2018-12-08T23:30:00+00:00");
+    const result1 = dateCookieObj.getAllResultForSpecificDate("2018-12-07");
+    const result2 = dateCookieObj.getAllResultForSpecificDate("2018-12-08");
+    expect(result1).toEqual([
+      {
+        cookie: "4sMM2LxV07bPJzwf",
+        count: 2,
+      },
+    ]);
+    expect(result2).toEqual([
+      {
+        cookie: "4sMM2LxV07bPJzwf",
+        count: 1,
+      },
+    ]);
+  });
+  it("should return null for non-existing date", () => {
+    const dateCookieObj = singletonDateCookie.getInstance();
+    dateCookieObj.clearInstance();
+    dateCookieObj.addCookieLine("4sMM2LxV07bPJzwf,2018-12-07T23:30:00+00:00");
+    const result = dateCookieObj.getAllResultForSpecificDate("2018-12-08");
+
+    expect(result).toBe(null);
+  });
 });
